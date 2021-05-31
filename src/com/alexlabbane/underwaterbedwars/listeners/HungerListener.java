a465240347464462e49349dfeaa922c1ac62047f
alexlabbane <alex.labbane@tamu.edu>
2021-05-30 23:58:07 -0500

    add javadoc comments to HungerListener

--- src/com/alexlabbane/underwaterbedwars/listeners/HungerListener.java
+++ src/com/alexlabbane/underwaterbedwars/listeners/HungerListener.java
@@ -9,7 +9,16 @@ import org.bukkit.event.entity.FoodLevelChangeEvent;
 
 import com.alexlabbane.underwaterbedwars.UnderwaterBedwars;
 
+/**
+ * Listener to prevent players from losing hunger
+ * @author Alex Labbane
+ *
+ */
 public class HungerListener implements Listener {
+	/**
+	 * When a player is about to lose hunger, cancel the event
+	 * @param event	the event being handled
+	 */
 	@EventHandler
 	public void onLoseHunger(FoodLevelChangeEvent event) {
 		Player player = null;
